08e4611524bea49c951f71b517f87ec55be87c75
JavierDevPro <gjangsam@gmail.com>
2025-06-24 12:55:11 -0500

    Adicion del objeto base con productos y metodos exploratorios

--- js/gestion_datos.js
+++ js/gestion_datos.js
@@ -1,3 +1,19 @@
 const productos = {
-    
-}
+    1:{
+        id: 1, nombre: "Laptop", precio: 1500
+    },
+    2:{
+        id: 2, nombre: "Smartphone", precio: 800
+    },
+    3:{
+        id: 3, nombre: "Tablet", precio: 600
+    },
+    4:{
+        id: 4, nombre: "Smartwatch", precio: 300
+    }
+};
+
+console.log(Object.keys(productos)); // Muestra las claves del objeto productos
+console.log(productos[4]); // Muestra el producto con id 4
+console.log(productos[4].nombre); // Muestra el nombre del producto con id 4
+console.log(productos[4].precio); // Muestra el precio del producto con id 4
